8cf14a97b008877656860267b3943b1678ea272e
eXpire163 <git@mms2k.de>
2019-02-19 22:03:51 +0100

    fix merge bug

--- Assets/Scripts/Intro/MenuManager.cs
+++ Assets/Scripts/Intro/MenuManager.cs
@@ -5,50 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
-<<<<<<< HEAD
+
 	public enum Maps{ expire, town, cs }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-=======
+
     [SerializeField]
     private SceneLoader sceneLoader;
->>>>>>> e38d2e7943fb1fb98c39ab18c5db4fdba01dd379
+
 
 	public void loadEinfach(){
 
 		GameSettings.Difficulty = GameSettings.GameDifficulty.Easy;
-<<<<<<< HEAD
 
-=======
         sceneLoader.LoadLevel("Level/country side");
->>>>>>> e38d2e7943fb1fb98c39ab18c5db4fdba01dd379
 
 	}
 
 	public void loadMittel(){
 		GameSettings.Difficulty = GameSettings.GameDifficulty.Medium;
-<<<<<<< HEAD
 
-=======
-        sceneLoader.LoadLevel("Level/town");
->>>>>>> e38d2e7943fb1fb98c39ab18c5db4fdba01dd379
+        sceneLoader.LoadLevel("Level/country side");
 
 	}
 
 	public void loadSchwer(){
 		GameSettings.Difficulty = GameSettings.GameDifficulty.Hard;
-<<<<<<< HEAD
-=======
+
         sceneLoader.LoadLevel("Level/town");
->>>>>>> e38d2e7943fb1fb98c39ab18c5db4fdba01dd379
+
+      
+
 
 	}
 
